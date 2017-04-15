6dd0bdc1d361de2633b70046853c532e7da3768c
Rizkiyana Prima Putra <rizkiyanapp@gmail.com>
2017-04-16 00:40:39 +0700

    Init

---
+++ GE_Kelompok_2.py
@@ -0,0 +1,20 @@
+# Author	:	Rizkiyana Prima Putra
+#				Muhammad Iqbal Tawakal
+#				Raditya Budi Handoko
+#				Nikho Toga Bungaran S
+# Date		: 	15 April 2017
+# Desc		: 	Implements Grammatical Evolultion
+#				for predicting average of gold price
+
+
+# LIBRARY
+import numpy as np
+import math as mt
+from openpyxl import load_workbook
+
+# KAMUS DATA
+
+
+# FUNGSI UNTUK MEMBACA FILE
+def loadData(filename):
+	
