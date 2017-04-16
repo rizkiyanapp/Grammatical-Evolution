44155918bac5cd4336a69f61cc18c55598b5895e
Rizkiyana Prima Putra <rizkiyanapp@gmail.com>
2017-04-16 10:40:12 +0700

    update

--- GE_Kelompok_2.py
+++ GE_Kelompok_2.py
@@ -8,13 +8,47 @@
 
 
 # LIBRARY
+from random import randint
+import random
 import numpy as np
 import math as mt
-from openpyxl import load_workbook
-
-# KAMUS DATA
+import xlrd
 
 
 # FUNGSI UNTUK MEMBACA FILE
-def loadData(filename):
-	
+def loadData(filename, sheetNum, colNum, data):
+	workbook = xlrd.open_workbook(filename)
+	sheet = workbook.sheet_by_index(sheetNum)
+	data = [val.value for val in sheet.col(colNum)]
+	data = data[1:]
+
+# FUNGSI MEMBANGKITKAN POPULASI SEBANYAK nPop KROMOSOM sebanyak nGen GEN
+def generatePopulation(population, nPop, nGen, nBit):
+	for x in range(nPop):
+		population.update({"Chromosome-%d" % (x+1) : [[random.randint(0,1) for x in range(nBit)] for y in range(nGen)]})
+	return population
+
+# FUNGSI UNTUK MENG-OUTPUT-KAN SETIAP KROMOSOM PADA POPULASI
+def showPopulation(population):
+	for key in population.keys():
+		print (key + " : " + str(population[key]))
+
+
+
+# KAMUS DATA
+data_train = []
+population = {}
+nBit = 8
+nGen = 10
+nPop = 100
+
+
+# MAIN PROGRAM
+# loadData("DataHistorisANTAM.xlsx", 0, 11, data_train)
+
+population = generatePopulation(population, nPop, nGen, nBit)
+showPopulation(population)
+
+
+
+# TEST CODE
