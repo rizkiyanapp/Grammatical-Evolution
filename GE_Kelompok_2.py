cbc2d621c071261c6476c42ed37412e93f26867e
Rizkiyana Prima Putra <rizkiyanapp@gmail.com>
2017-04-16 13:14:55 +0700

    update

--- GE_Kelompok_2.py
+++ GE_Kelompok_2.py
@@ -15,6 +15,13 @@ import math as mt
 import xlrd
 
 
+
+def toDecimal(binarys):
+	total = 0
+	for x in range(len(binarys)):
+		total += (2**x)*list(reversed(binarys))[x]
+	return total
+
 # FUNGSI UNTUK MEMBACA FILE
 def loadData(filename, sheetNum, colNum, data):
 	workbook = xlrd.open_workbook(filename)
@@ -26,6 +33,15 @@ def loadData(filename, sheetNum, colNum, data):
 def generatePopulation(population, nPop, nGen, nBit):
 	for x in range(nPop):
 		population.update({"Chromosome-%d" % (x+1) : [[random.randint(0,1) for x in range(nBit)] for y in range(nGen)]})
+	return population
+
+# FUNGSI UNTUK KONVERSI KROMOSOM BINER KE KROMOSOM INTEGER
+def populationConvert(population):
+	for key in population.keys():
+		newGen = []
+		for val in population[key]:
+			newGen.append(toDecimal(val))
+		population[key] = newGen
 	return population
 
 # FUNGSI UNTUK MENG-OUTPUT-KAN SETIAP KROMOSOM PADA POPULASI
@@ -42,11 +58,19 @@ nBit = 8
 nGen = 10
 nPop = 100
 
+productionRules = {
+	'expr' : [['expr', 'op', 'expr'], ['(', 'expr', 'op', 'expr', ')'], ['pre_op', 'expr'], 'var'],
+	'op' : ['-', '+', '/', '*'],
+	'pre_op' : ['sin', 'cos', 'tan', 'log'],
+	'var' : 'x'
+}
+
 
 # MAIN PROGRAM
 # loadData("DataHistorisANTAM.xlsx", 0, 11, data_train)
 
 population = generatePopulation(population, nPop, nGen, nBit)
+population = populationConvert(population)
 showPopulation(population)
 
 
